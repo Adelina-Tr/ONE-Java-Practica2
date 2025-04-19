603f49bbccf2164b2ecd712969db76bc0124aebb
Adelina Trejo <kf8@hotmail.com>
2025-04-19 14:45:47 -0600

    Herencia mediante palabra clave 'extends'

--- src/com/aluracursos/screenmatch/modelos/Pelicula.java
+++ src/com/aluracursos/screenmatch/modelos/Pelicula.java
@@ -1,67 +1,15 @@
 //Indica el paquete al que esta clase pertenece
 package com.aluracursos.screenmatch.modelos;
 
-public class Pelicula {
+public class Pelicula extends Titulo{
 
-    //Propiedades de la clase
-    private String nombre;
-    private int fechaDeLanzamiento;
-    private int duracionEnMinutos;
-    private boolean inclidoEnElPlan;
-    private double sumaDeEvaluaciones;
-    private int totalDeEvaluaciones;
+    private String director;
 
-
-    //Generacion de Getters
-    public String getNombre() {
-        return nombre;
+    public String getDirector() {
+        return director;
     }
 
-    public int getFechaDeLanzamiento() {
-        return fechaDeLanzamiento;
-    }
-
-    public int getDuracionEnMinutos() {
-        return duracionEnMinutos;
-    }
-
-    public boolean isInclidoEnElPlan() {
-        return inclidoEnElPlan;
-    }
-    public int getTotalDeEvaluaciones(){
-        return totalDeEvaluaciones;
-    }
-
-    //Generacion de Setters
-    public void setFechaDeLanzamiento(int fechaDeLanzamiento) {
-        this.fechaDeLanzamiento = fechaDeLanzamiento;
-    }
-
-    public void setDuracionEnMinutos(int duracionEnMinutos) {
-        this.duracionEnMinutos = duracionEnMinutos;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public void setInclidoEnElPlan(boolean inclidoEnElPlan) {
-        this.inclidoEnElPlan = inclidoEnElPlan;
-    }
-
-    //Metodo que imprime los valores de cada atributo de la clase
-    public void muestraFichaTecnica(){
-        System.out.println("El nombre de la pelicula es: "+nombre);
-        System.out.println("Su fecha de lanzamiento es: "+fechaDeLanzamiento);
-        System.out.println("Su duracion en minutos es "+duracionEnMinutos);
-    }
-    //Metodo que aniade notas tipo doble
-    public void evalua(double nota){
-        sumaDeEvaluaciones += nota;
-        totalDeEvaluaciones++;
-    }
-    //Metodo que calcula el promedio del total de notas
-    public double calculaMedia(){
-        return sumaDeEvaluaciones/totalDeEvaluaciones;
+    public void setDirector(String director) {
+        this.director = director;
     }
 }
---
+++ src/com/aluracursos/screenmatch/modelos/Serie.java
@@ -0,0 +1,32 @@
+package com.aluracursos.screenmatch.modelos;
+
+public class Serie extends Titulo{
+
+    private int temporadas;
+    private int episodiosPorTemporadas;
+    private int minutosPorEpisodio;
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+
+    public int getEpisodiosPorTemporadas() {
+        return episodiosPorTemporadas;
+    }
+
+    public void setEpisodiosPorTemporadas(int episodiosPorTemporadas) {
+        this.episodiosPorTemporadas = episodiosPorTemporadas;
+    }
+
+    public int getMinutosPorEpisodio() {
+        return minutosPorEpisodio;
+    }
+
+    public void setMinutosPorEpisodio(int minutosPorEpisodio) {
+        this.minutosPorEpisodio = minutosPorEpisodio;
+    }
+}
